a7c4caece4b1be43fc6e7e9f4a2b885307cdc097
Sheikh Nasim Limon <158989630+NasimLimon@users.noreply.github.com>
2024-08-29 09:56:11 +0600

    Update CF problem(DIJKSTRA).cpp

--- CF problem(DIJKSTRA).cpp
+++ CF problem(DIJKSTRA).cpp
@@ -64,3 +64,90 @@ int main()
 
 	return 0;
 }
+
+
+
+ORRR
+///   ***   ---   |||   In the name of ALLAH   |||   ---   ***   ///
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+
+
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+const int mx = 1e5 + 123;
+vector<pair<long long, long long>> adj[mx];
+ll dist[mx];
+int parent[mx];
+
+void dijkstra(int s, int n) {
+    for (int i = 1; i <= n; i++) {
+        dist[i] = infLL;
+        parent[i] = -1;
+    }
+    
+    dist[s] = 0;
+    priority_queue<pair<long long, long long>, vector<pair<long long, long long>>, greater<pair<long long, long long>>> pq;
+    pq.push({0, s});
+
+    while (!pq.empty()) {
+        ll curD = pq.top().first;
+        int u = pq.top().second;
+        pq.pop();
+
+        if (dist[u] < curD) continue;
+
+        for (auto p : adj[u]) {
+            int v = p.first;
+            ll weight = p.second;
+
+            if (curD + weight < dist[v]) {
+                dist[v] = curD + weight;
+                parent[v] = u;
+                pq.push({dist[v], v});
+            }
+        }
+    }
+}
+
+vector<int> get_path(int n) {
+    vector<int> path;
+    while (n != -1) {
+        path.push_back(n);
+        n = parent[n];
+    }
+    reverse(path.begin(), path.end());
+    return path;
+}
+
+int main() {
+    optimize();
+
+    int n, m;
+    cin >> n >> m;
+    for (int i = 1; i <= m; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        adj[u].PB({v, w});
+        adj[v].PB({u, w});
+    }
+
+    dijkstra(1, n);
+
+    if (dist[n] == infLL) {
+        cout << -1 << endl;
+    } else {
+        vector<int> path = get_path(n);
+        for (int i = 0; i < path.size(); i++) {
+            cout << path[i] << " ";
+        }
+    }
+    cout << endl;
+
+    return 0;
+}
