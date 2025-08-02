b58a91652012968f05060456bec5510e714bbf85
Nasim Limon <nurshakiblimon2018@gmail.com>
2025-08-02 21:05:57 +0600

    add on new file

---
+++ new-file/Coin Combinations I.cpp
@@ -0,0 +1,31 @@
+//                                           IN THE NAME OF SUPREME & MERCIFUL GOD
+//                                               Bismillahir Rahmanir Rahim
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define endl '\n'
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define file()                        \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+ll gcd(ll a, ll b) { return __gcd(a, b); }
+ll lcm(ll a, ll b) { return a * (b / gcd(a, b)); }
+int dx[] = {1, -1, 0, 0, 1, 1, -1, -1};
+int dy[] = {0, 0, 1, -1, 1, -1, -1, 1};
+int main()
+{
+    optimize();
+    long long t;
+    cin >> t;
+    while (t--)
+    {
+    }
+}
