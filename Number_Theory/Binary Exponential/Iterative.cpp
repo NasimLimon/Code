f726e3a25ab689b3f559babb1ec376a0e816d6c1
Sheikh Nasim Limon <158989630+NasimLimon@users.noreply.github.com>
2025-09-15 13:43:05 +0600

    Update Iterative.cpp

--- Number_Theory/Binary Exponential/Iterative.cpp
+++ Number_Theory/Binary Exponential/Iterative.cpp
@@ -1,1 +1,26 @@
 
+
+// Iterative approach
+ll BinaryIter(ll a, ll b)
+{
+    ll ans = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+        {
+             ans = (ans * a) % MOD;
+        }
+         a = (a * a) % MOD;
+        b = b >> 1;
+    }
+    return ans;
+}
+
+int main()
+{
+    optimize();
+    ll n, m;
+    cin >> n >> m;
+    cout << BinaryIter(n, m) << endl;
+}
+
