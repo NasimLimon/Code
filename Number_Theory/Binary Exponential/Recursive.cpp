ea4f3d102cddc5e2ef84daaad56b2b426e90d7af
Sheikh Nasim Limon <158989630+NasimLimon@users.noreply.github.com>
2025-09-15 13:41:35 +0600

    Update Recursive.cpp

--- Number_Theory/Binary Exponential/Recursive.cpp
+++ Number_Theory/Binary Exponential/Recursive.cpp
@@ -1,1 +1,23 @@
+// Recursive Approach
+ll BinaryExpo(ll a, ll b)
+{
+    if (b == 0)
+        return 1;
+    ll res = BinaryExpo(a, b / 2);
+    if (b % 2 != 0)
+    {
+        return (a * (res * res) % MOD) % MOD;
+    }
+    else
+    {
+        return ((res * res) % MOD) % MOD;
+    }
+}
+int main()
+{
+    optimize();
+    ll n, m;
+    cin >> n >> m;
+    cout << BinaryExpo(n, m) << endl;
+}
 
