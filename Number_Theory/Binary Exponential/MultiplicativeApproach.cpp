d504f8e2e10ec000ab9af52e1e3fe70de93e8381
Sheikh Nasim Limon <158989630+NasimLimon@users.noreply.github.com>
2025-09-15 13:45:27 +0600

    Update MultiplicativeApproach.cpp

--- Number_Theory/Binary Exponential/MultiplicativeApproach.cpp
+++ Number_Theory/Binary Exponential/MultiplicativeApproach.cpp
@@ -1,1 +1,40 @@
 
+// BinaryMultiplication
+ll BinaryMultiplication(ll a, ll b)
+{
+    ll ans = 0;
+    while (b > 0)
+    {
+        if (b & 1)
+        {
+            ans = (ans + a) % MOD;
+        }
+        a = (a + a) % MOD;
+        b = b >> 1;
+    }
+    return ans;
+}
+// Iterative approach
+ll BinaryIter(ll a, ll b)
+{
+    ll ans = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+        {
+            ans = BinaryMultiplication(ans, a);
+        }
+        a = BinaryMultiplication(a, a);
+        b = b >> 1;
+    }
+    return ans;
+}
+
+int main()
+{
+    optimize();
+    ll n, m;
+    cin >> n >> m;
+    cout << BinaryIter(n, m) << endl;
+}
+
