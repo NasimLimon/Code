9eb0bf2021ad4c0bb088daf02f6b5806f6f924ca
Sheikh Nasim Limon <158989630+NasimLimon@users.noreply.github.com>
2025-11-30 19:59:17 +0000

    new

---
+++ MinCostClimbingStairsLC.cpp
@@ -0,0 +1,43 @@
+//                                           IN THE NAME OF SUPREME & MERCIFUL GOD
+//                                               Bismillahir Rahmanir Rahim
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define endl '\n'
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+
+#define Limon()                   \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define file()                        \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+ll gcd(ll a, ll b) { return __gcd(a, b); }
+ll lcm(ll a, ll b) { return a * (b / gcd(a, b)); }
+int dx[] = {1, -1, 0, 0, 1, 1, -1, -1};
+int dy[] = {0, 0, 1, -1, 1, -1, -1, 1};
+
+
+int MinCostStair(vector<int>&v){
+ 
+}
+int main()
+{
+    Limon();
+    int n;
+    cin >> n;
+    vector<int> v1;
+    int count = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        int x;
+        cin >> x;
+        v1.push_back(x);
+    }
+    cout << MinCostStair(v1) << endl;
+}
