c75afbcfa6658a85080badc24c5857b9383e3e34
Sheikh Nasim Limon <158989630+NasimLimon@users.noreply.github.com>
2025-12-01 02:06:12 +0600

    new

--- MinCostClimbingStairsLC.cpp
+++ MinCostClimbingStairsLC.cpp
@@ -21,10 +21,22 @@ ll gcd(ll a, ll b) { return __gcd(a, b); }
 ll lcm(ll a, ll b) { return a * (b / gcd(a, b)); }
 int dx[] = {1, -1, 0, 0, 1, 1, -1, -1};
 int dy[] = {0, 0, 1, -1, 1, -1, -1, 1};
-
-
-int MinCostStair(vector<int>&v){
- 
+int dp[100 + 5];
+int a[2] = {1, 2};
+int MinCostStair(vector<int> &v, int siz)
+{
+    if (siz == 0)
+        return v[0];
+    int mx = 2000000000;
+    for (int i = 0; i < 2; i++)
+    {
+        if (siz - a[i] >= 0)
+        {
+            mx = min(mx, MinCostStair(v, siz - a[i]));
+        }
+    }
+     dp[siz] = mx;
+    return dp[siz];
 }
 int main()
 {
@@ -32,12 +44,13 @@ int main()
     int n;
     cin >> n;
     vector<int> v1;
-    int count = 0;
-    for (int i = 1; i <= n; i++)
+for (int i = 1; i <= n; i++)
     {
         int x;
         cin >> x;
         v1.push_back(x);
     }
-    cout << MinCostStair(v1) << endl;
+    for (int i = 0; i < 104; i++)
+        dp[i] = -1;
+    cout << MinCostStair(v1, v1.size() + 1) << endl;
 }
